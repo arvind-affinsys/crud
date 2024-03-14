2da1b114189eea8e888e7165893ec983ca05505e
Arvind A <arvind.a@affinsys.com>
2024-03-14 18:04:09 +0530

    Updated: to dynamically save to db

--- api/middleware.py
+++ api/middleware.py
@@ -1,7 +1,22 @@
-from django.conf import settings
+import logging
 from django.http import HttpRequest, HttpResponse
 
 from api.models import Log
+
+
+class CustomLogHandler(logging.Handler):
+    def emit(self, record):
+        try:
+            log = Log(**record.__dict__["msg"])
+            log.save()
+        except Exception as e:
+            print("EXCEPTION: ", e)
+
+
+logging.basicConfig(handlers=[CustomLogHandler()], level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 
 class CustomMiddleware:
     def __init__(self, get_response):
@@ -9,28 +24,27 @@ class CustomMiddleware:
 
     def __call__(self, request: HttpRequest):
         print("Custom Middleware Called")
-        if not request.path.startswith('/api/todos/'):
+        if not request.path.startswith("/api/todos/"):
             return self.get_response(request)
-        request_body = request.body.decode('utf-8')
+        request_body = request.body.decode("utf-8")
         headers = str(request.headers)
         method = request.method
         path = request.path
         user = request.user.username
-        ip_address = request.META.get('REMOTE_ADDR')
+        ip_address = request.META.get("REMOTE_ADDR")
         params = {
-            'request_body': request_body,
-            'headers': headers,
-            'method': method,
-            'path': path,
-            'user': user,
-            'ip_address': ip_address,
+            "request_body": request_body,
+            "headers": headers,
+            "method": method,
+            "path": path,
+            "user": user,
+            "ip_address": ip_address,
         }
         response: HttpResponse = self.get_response(request)
-        params['status'] = response.status_code
-        params['response_body'] = response.content.decode('utf-8')
+        params["status"] = response.status_code
+        params["response_body"] = response.content.decode("utf-8")
         try:
-            log = Log(**params)
-            log.save()
+            logger.info(params)
         except Exception as e:
             print("EXCEPTION: ", e)
         return response
